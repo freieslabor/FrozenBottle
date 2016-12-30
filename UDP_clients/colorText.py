fb72d6ca1a56b7032f12268101f0eee64efc0161
Steffen Trumtrar <mail@netploitation.de>
2016-12-30 11:05:00 +0100

    colorText: pep8-ify
    Signed-off-by: Steffen Trumtrar <mail@netploitation.de>

--- UDP_clients/colorText.py
+++ UDP_clients/colorText.py
@@ -2,7 +2,6 @@
 
 # program to send some blinking to UDP for the flozen-bottle setup.
 
-
 import argparse
 import math
 import os.path
@@ -13,110 +12,112 @@ import hex
 
 DEFAULT_PORT = 8901
 
+
 def main(args):
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("address",type=str,help="UDP address")
-        parser.add_argument("text",type=str,help="Text")
-	parser.add_argument("-p","--port",type=int,help="UDP port number")
-	parser.add_argument("-r","--revert",default=False,action="store_true")
-	aa = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("address", type=str, help="UDP address")
+    parser.add_argument("text", type=str, help="Text")
+    parser.add_argument("-p", "--port", type=int, help="UDP port number")
+    parser.add_argument("-r", "--revert", default=False, action="store_true")
+    aa = parser.parse_args()
 
-	print repr(aa)
+    print repr(aa)
 
-	port = DEFAULT_PORT
-	address = "127.0.0.1"
-	if aa.port is not None:
-		port = aa.port
+    port = DEFAULT_PORT
+    address = "127.0.0.1"
+    if aa.port is not None:
+        port = aa.port
 
-	if port<=0 or port==0xFFFF:
-		sys.stderr.write("bad port number %u\n"%port)
-		return 1
+    if port <= 0 or port == 0xFFFF:
+        sys.stderr.write("bad port number %u\n" % port)
+        return 1
 
-	if aa.address is not None:
-		address = aa.address
+    if aa.address is not None:
+        address = aa.address
 
-	if not LedClientBase.connect(address,port):
-		return 1
+    if not LedClientBase.connect(address, port):
+        return 1
 
-        mainbuff = hex.HexBuff(20,14,0,0,(0.0,0.0,0.0))
-        mainbuff.fill_val((0.2,0.2,0.2))
-        l = list()
-        for i in range(len(aa.text)):
-            if (aa.text[i] == "/"):
-                l.append(read_hex_file(os.path.join("data","slash.hex")))
-            elif(aa.text[i] == "."):
-                l.append(read_hex_file(os.path.join("data","dot.hex")))
+    mainbuff = hex.HexBuff(20, 14, 0, 0, (0.0, 0.0, 0.0))
+    mainbuff.fill_val((0.2, 0.2, 0.2))
+    l = list()
+    for i in range(len(aa.text)):
+        if (aa.text[i] == "/"):
+            l.append(read_hex_file(os.path.join("data", "slash.hex")))
+        elif (aa.text[i] == "."):
+            l.append(read_hex_file(os.path.join("data", "dot.hex")))
+        else:
+            l.append(read_hex_file(os.path.join("data", aa.text[i] + ".hex")))
+
+    t = 0.0
+    length = 13 + len(l) * 7
+
+    for i in xrange(0x7FFF0000):
+        ii = (i % length)
+        distance = 13
+        mainbuff.fill_val((0, 0, 0.01))
+        for j in range(len(l)):
+            if (aa.revert):
+                mainbuff.blit(l[j], distance - ii, 4, hex.HexBuff.XFORM_FLIP_X)
             else:
-                l.append(read_hex_file(os.path.join("data",aa.text[i]+".hex")))
+                mainbuff.blit(l[j], distance - ii, 4, None)
+            distance += 7
+
+        lin = list()
+        for j in xrange(LedClientBase.NUMLEDS):
+            (xx, yy) = LedClientBase.seq_2_pos(j)
+            rgb_tuple = mainbuff.get_xy(xx, yy)
+            if (rgb_tuple == (1, 1, 1)):
+                rgb_tuple = (0, 0, 0)
+            if (rgb_tuple == (0, 0, 0.01)):
+                rgb_tuple = color_calc_func_1(i, j, xx, yy)
+
+            lin.append(LedClientBase.rgbF_2_bytes(rgb_tuple))
+        LedClientBase.send("".join(lin))
+
+        time.sleep(0.03)
+        t += 0.03
+
+    LedClientBase.closedown()
+
+    return 0
 
 
+def color_calc_func_1(frameNo, seq_id, xx, yy):
+    dot = (frameNo // 7) % LedClientBase.NUMLEDS
+    t = 0.017 * frameNo
+    if (frameNo & 128) == 0:
+        col_cyc_idx = xx
+    else:
+        col_cyc_idx = yy
+    #if dot==seq_id:
+    #	return (1.0,1.0,1.0)
+    return LedClientBase.hsv2rgb_float(
+        ((t + 0.125 * col_cyc_idx) / 3.0) % 1.0, 0.75, 1.0)
 
-	t = 0.0
-	length = 13 + len(l)*7
-
-	for i in xrange(0x7FFF0000):
-                ii = (i%length)
-	        distance = 13
-		mainbuff.fill_val((0,0,0.01))
-                for j in range(len(l)):
-                    if (aa.revert):
-                        mainbuff.blit(l[j],distance - ii,4,hex.HexBuff.XFORM_FLIP_X)
-                    else:
-                        mainbuff.blit(l[j],distance - ii,4,None)
-                    distance += 7
-
-
-		lin = list()
-		for j in xrange(LedClientBase.NUMLEDS):
-			(xx,yy) = LedClientBase.seq_2_pos(j)
-			rgb_tuple = mainbuff.get_xy(xx,yy)
-			if (rgb_tuple == (1,1,1)):
-				rgb_tuple = (0,0,0)
-			if (rgb_tuple == (0,0,0.01)):
-				rgb_tuple = color_calc_func_1(i,j,xx,yy)
-
-			lin.append(LedClientBase.rgbF_2_bytes(rgb_tuple))
-		LedClientBase.send("".join(lin))
-
-		time.sleep(0.03)
-		t += 0.03
-
-	LedClientBase.closedown()
-
-	return 0
-
-
-
-def color_calc_func_1(frameNo,seq_id,xx,yy):
-	dot = (frameNo//7)%LedClientBase.NUMLEDS
-	t = 0.017*frameNo
-	if (frameNo&128)==0:
-		col_cyc_idx = xx
-	else:
-		col_cyc_idx = yy
-	#if dot==seq_id:
-	#	return (1.0,1.0,1.0)
-	return LedClientBase.hsv2rgb_float(((t+0.125*col_cyc_idx)/3.0)%1.0,0.75,1.0)
 
 def read_hex_file(name):
-	v = hex.read_hex_file(name,True)
-	w0,w1,h0,h1 = 0x10000,0,0x10000,0
-	for x,y in v:
-		h = y>>1
-		w = ((x+h)>>1)
-		w0=min(w,w0);w1=max(w,w1)
-		h0=min(h,h0);h1=max(h,h1)
-	res = hex.HexBuff(w1+1-w0,h1+1-h0,0,0,None)
-	#res.fill_val((0.0,0.0,0.0))
-	(ox,oy) = res.wh2xy(w,h)
-	for x,y in v:
-		h = y>>1
-		w = ((x+h)>>1)
-		col = v[(x,y)]
-		if col is not None:
-			res.set_wh(w-w0,h-h0,col)
-	return res
+    v = hex.read_hex_file(name, True)
+    w0, w1, h0, h1 = 0x10000, 0, 0x10000, 0
+    for x, y in v:
+        h = y >> 1
+        w = ((x + h) >> 1)
+        w0 = min(w, w0)
+        w1 = max(w, w1)
+        h0 = min(h, h0)
+        h1 = max(h, h1)
+    res = hex.HexBuff(w1 + 1 - w0, h1 + 1 - h0, 0, 0, None)
+    #res.fill_val((0.0,0.0,0.0))
+    (ox, oy) = res.wh2xy(w, h)
+    for x, y in v:
+        h = y >> 1
+        w = ((x + h) >> 1)
+        col = v[(x, y)]
+        if col is not None:
+            res.set_wh(w - w0, h - h0, col)
+    return res
 
-if __name__=="__main__":
-	sys.exit(main(sys.argv[1:]))
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
